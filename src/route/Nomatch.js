61251a3499a1de233597a6d49d468d4549ae1e73
AntoineDrsl <antoine.durussel@gmail.com>
2021-02-04 19:11:32 +0100

    [FEAT] nomatch page

---
+++ src/route/Nomatch.js
@@ -0,0 +1,11 @@
+import '../Sass/components/_nomatch.scss';
+
+function Nomatch() {
+    return(
+        <div className="noMatch">
+            <p>Cette page n'existe pas :{'('}</p>
+        </div>
+    )
+}
+
+export default Nomatch;
--- src/route/Routing.js
+++ src/route/Routing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from '../components/Header';
+import Nomatch from './Nomatch';
 
 function Routing() {
     return(
@@ -8,11 +9,14 @@ function Routing() {
             <Header/>
             <main className="App-body">
                 <Switch>
-                    <Route path="/">
+                    <Route exact path="/">
 
                     </Route>
                     <Route path="/quizz">
 
+                    </Route>
+                    <Route path="*">
+                        <Nomatch/>
                     </Route>
                 </Switch>
             </main>
